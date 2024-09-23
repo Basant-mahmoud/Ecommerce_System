6123046a750ca4389ed6e6fdf9299ca248e9bbf7
Basant Mahmoud <103046859+Basant-mahmoud@users.noreply.github.com>
2024-09-23 03:08:01 +0300

    Payment EndPoint

---
+++ Ecommerce.Application/InterfacesServices/IPaymentService.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce_System.Ecommerce.Domain.DTO;
+using Ecommerce_System.Ecommerce.Domain.Models;
+
+namespace Ecommerce_System.Ecommerce.Application.InterfacesServices
+{
+    public interface IPaymentService
+    {
+        Task<Payment> CreateAsync(PaymnetDto payment);
+        Task<Payment> UpdateAsync(PaymnetDto payment);
+        Task DeleteAsync(int paymentId);
+        Task<Payment> GetPaymentByIdAsync(int PaymentId);
+        Task<IEnumerable<Payment>> GetAllPaymentsAsync();
+        Task<Payment> GetUserPaymentAsync(int orderId);
+    }
+}
---
+++ Ecommerce.Domain/InterfacesRepo/IPaymentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce_System.Ecommerce.Domain.Models;
+
+namespace Ecommerce_System.Ecommerce.Domain.InterfacesRepo
+{
+    public interface IPaymentRepository
+    {
+        Task <Payment> CreateAsync (Payment payment);
+        Task<Payment> UpdateAsync (Payment payment);
+        Task<Payment> DeleteAsync(Payment payment);
+        Task<Payment> GetPaymentByIdAsync(int PaymentId);
+        Task<IEnumerable<Payment>> GetAllPaymentAsync();
+        Task<Payment> GetUserPayment(int OrderId);
+    }
+}
